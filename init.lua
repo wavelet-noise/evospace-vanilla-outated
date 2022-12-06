9be19ff3841957793761bb56209f9f419ba11f6c
wavelet-noise <ishellstrike@gmail.com>
2022-12-06 21:34:57 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+function on_mod_pre_init()
+    print("im a mod 1")
+end 
+
+function on_mod_init() 
+    print("im a mod 2")    
+    print(1+2)
+end
+
+function on_mod_post_init()
+    print("im a mod 3")
+end
